dcb1521ac96e29aa73c052eadfff2fb5c82ffd90
Nero1110 <nourelmoustafa123@gmail.com>
2025-12-15 18:31:59 +0200

    add note bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/constants/constants.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/screens/notes_screen.dart';
 import 'package:hive_ce_flutter/hive_flutter.dart';
@@ -16,27 +18,32 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        textTheme: TextTheme(
-          bodyLarge: TextStyle(
-            fontSize: 25,
-            fontFamily: "Poppins",
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
+    return MultiBlocProvider(
+      providers: [
+BlocProvider(create: (context)=>AddNoteCubit())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          textTheme: TextTheme(
+            bodyLarge: TextStyle(
+              fontSize: 25,
+              fontFamily: "Poppins",
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+            bodyMedium: TextStyle(
+              fontFamily: "Poppins",
+              fontSize: 18,
+              color: Colors.black,
+              fontWeight: FontWeight.w400,
+            ),
+            headlineLarge: TextStyle(fontSize: 27, fontFamily: "Poppins"),
           ),
-          bodyMedium: TextStyle(
-            fontFamily: "Poppins",
-            fontSize: 18,
-            color: Colors.black,
-            fontWeight: FontWeight.w400,
-          ),
-          headlineLarge: TextStyle(fontSize: 27, fontFamily: "Poppins"),
         ),
+        home: const NotesScreen(),
       ),
-      home: const NotesScreen(),
     );
   }
 }
