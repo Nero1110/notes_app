5aea38e291bb511b2c51a93e08e1e7942610ac0e
Nero1110 <nourelmoustafa123@gmail.com>
2025-12-15 18:29:44 +0200

    add note method in add note cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,13 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constants/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-  void addNote({required NoteModel notemodel}){
-
-    
+  void addNote({required NoteModel notemodel}) async {
+    try {
+      emit(AddNoteLoading());
+      var notebox = notesBox;
+      await notebox.add(notemodel);
+      emit(AddNoteSuccess());
+    } catch (e) {
+      emit(AddNoteFailure(errormessage: e.toString()));
+      
+    }
   }
 }
